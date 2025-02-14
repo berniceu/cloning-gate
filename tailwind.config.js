cdb843725cf2f71048ffc28bacd5615fba4be6fa
uwituzeb <b.uwituze@alustudent.com>
2025-02-14 13:45:22 +0200

    style second section mobile view

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,9 @@ module.exports = {
         'custom-blue': '#0c2340',
         'custom-dark-blue': '#081629',
         'custom-grey': '#555',
-        'custom-light-blue': '#143865'
+        'custom-light-blue': '#143865',
+        'custom-sky-blue': '#edf2f9',
+        'custom-dark-sky-blue': '#c1cddd',
       },
       fontFamily: {
         sans: ['Libre Franklin', 'sans-serif'],
